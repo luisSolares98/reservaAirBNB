8db81e23318d296364995212b257cfd8f72d68d8
luis.solares <lmsolares@bg.com.bo>
2023-09-05 09:22:27 -0400

    exclude de testUnit

--- Domain/src/main/java/com/nur/model/Reserve.java
+++ Domain/src/main/java/com/nur/model/Reserve.java
@@ -1,13 +1,11 @@
 package com.nur.model;
 
-import com.nur.core.BussinessRuleValidationException;
 import com.nur.core.Entity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
-import java.util.UUID;
 @Getter
 @Setter
 @Builder
@@ -16,10 +14,4 @@ public class Reserve extends Entity {
     private String state;
     private Date dateOut;
 
-    public Reserve(UUID key, Date dateIn, String state, Date dateOut) {
-        this.key = key;
-        this.dateIn = dateIn;
-        this.state = state;
-        this.dateOut = dateOut;
-    }
 }
