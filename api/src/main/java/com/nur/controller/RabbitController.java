7b2b715af25eecc6e9723c0584f4f01d9e3e4f14
luis.solares <lmsolares@bg.com.bo>
2023-11-18 06:22:23 -0400

    rabbit

--- api/src/main/java/com/nur/controller/RabbitController.java
+++ api/src/main/java/com/nur/controller/RabbitController.java
@@ -2,6 +2,8 @@ package com.nur.controller;
 
 import com.nur.rabbit.Config;
 import com.nur.rabbit.CustomMessage;
+import com.nur.rabbit.Pattern;
+import com.nur.rabbit.Response;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,8 +23,15 @@ public class RabbitController {
 
     @PostMapping("/")
     public String publishMessage(@RequestBody CustomMessage message) {
-        message.setId(UUID.randomUUID());
-        template.convertAndSend(Config.EXCHANGE, message);
+
+        Pattern pattern = Pattern.builder().cmd(Config.EXCHANGE).build();
+
+        CustomMessage message2 = CustomMessage.builder().id(UUID.randomUUID())
+                .message(message.getMessage()).build();
+
+        Response response = Response.builder().data(message2).pattern(pattern).build();
+
+        template.convertAndSend(Config.EXCHANGE, response);
 
         return "Message Published";
     }
